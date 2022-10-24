11b253d31c4b877181b8be945f6dc3128a9b3754
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-10-24 12:11:14 +0600

    Establish a connection to SQL Server database

---
+++ Establish a connection to SQL Server database/Establish a connection to SQL Server database/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace Establish_a_connection_to_SQL_Server_database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectToServer();
+        }
+
+        public static void ConnectToServer()
+        {
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection("Server = DESKTOP-VDA92FM\\SQLEXPRESS; Database = University; Trusted_Connection = True; Encrypt = false;");
+                connection.Open();
+                Console.WriteLine("Connection established successfully");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Connection do not establish. Because of");
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+    }
+}
