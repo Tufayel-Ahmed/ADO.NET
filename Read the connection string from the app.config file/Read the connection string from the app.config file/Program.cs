05fd1deea42b467dcb1dd2f7243cbdf0b93a1771
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-10-25 13:33:07 +0600

    Read the connection string from the app.config file

---
+++ Read the connection string from the app.config file/Read the connection string from the app.config file/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Data.SqlClient;
+
+
+namespace Read_the_connection_string_from_the_app.config_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string ConString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(ConString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection Established Successfully");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
