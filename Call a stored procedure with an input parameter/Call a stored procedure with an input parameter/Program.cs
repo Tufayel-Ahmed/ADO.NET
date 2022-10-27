1a515d1c744cba7cf552f6ae7e1e618acb1e7c0f
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-10-27 15:00:27 +0600

    Call a stored procedure with an input and output parameter

---
+++ Call a stored procedure with an input parameter/Call a stored procedure with an input parameter/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace Call_a_stored_procedure_with_an_input_and_output_parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallStoreProcedure();
+        }
+
+        public static void CallStoreProcedure()
+        {
+            SqlConnection connection = null;
+
+            try
+            {
+                string conString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                connection = new SqlConnection(conString);
+
+                SqlCommand cmd = new SqlCommand()
+                {
+                    CommandText = "spCreateStudent",
+                    Connection = connection,
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                SqlParameter parameter1 = new SqlParameter()
+                {
+                    ParameterName = "@Name",
+                    SqlDbType = SqlDbType.NVarChar,
+                    Value = "Test",
+                    Direction = ParameterDirection.Input
+                };
+                cmd.Parameters.Add(parameter1);
+                cmd.Parameters.AddWithValue("@Email", "test@gmail.com");
+                cmd.Parameters.AddWithValue("Mobile", "123456789");
+
+                SqlParameter outParameter = new SqlParameter()
+                {
+                    ParameterName = "@Id",
+                    SqlDbType = SqlDbType.Int,
+                    Direction = ParameterDirection.Output
+                };
+                cmd.Parameters.Add(outParameter);
+
+                connection.Open();
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine($"Newely Generated Student ID: {outParameter.Value.ToString()}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+    }
+}
